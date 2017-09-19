1e822a2efa8a5159cdf3dd474da99c0645867917
KimBelanger <kim@kimberlybelanger.com>
2017-09-19 12:21:59 -0700

    meets expectations initial commit

---
+++ js/app.js
@@ -0,0 +1,85 @@
+// get all the students and put them into an array
+const students = document.getElementsByClassName("student-item");
+console.log("students", students.length);
+// get the page element to append items to
+const pageContainer = document.getElementById('page');
+
+
+/********** ADD PAGINATION FUNCITON **********/
+const addPagination = (studentList) => {
+    /** determine how many pages there should be based on number of students **/
+    let pageNum = studentList.length / 10;
+    if (pageNum % 1 != 0) { // if the number of students is not evenly divided by 10, add 1 to the number of pages
+        pageNum = Math.floor(pageNum) + 1;
+    } 
+    
+    /** build pagination **/
+    // create div to hold pagination items
+    const pagination = document.createElement('div');
+    pagination.className = "pagination";
+    pagination.id = "pagination";
+    
+    // create html sturcture by looping through the number of pages needed
+    let paginationHTML = '<ul>';
+    for (let i = 1; i <= pageNum; i++) {
+        paginationHTML += '<li><a href="#">'+i+'</a></li>';
+    }
+    paginationHTML += "</ul>"
+    pagination.innerHTML = paginationHTML; // set inner html to the unorderlist of page navs
+
+    pageContainer.appendChild(pagination); // append pagination to page
+}
+
+
+
+/********** DEFAULT DISPLAY WHEN PAGE LOADS **********/
+// hide all students by default
+for (let i = 0; i < students.length; i++) { 
+    students[i].style.display = "none";
+}
+
+// disiplay the first 10 students by default
+for (let i = 0; i < 10; i++) { 
+    students[i].style.display = "block";
+}
+// add pagination to page by default
+addPagination(students);
+
+// set first pagination button to active by default
+document.querySelector('#pagination ul li a').classList.add("active");
+
+
+
+/********** SHOW PAGE FUNCITON **********/
+const paginationUl = document.querySelector('#pagination ul');
+const paginationItems = document.querySelectorAll('#pagination ul li a');
+
+/** SHOW PAGE ITEMS ON CLICK **/
+paginationUl.addEventListener('click', (event) => {
+    event.preventDefault(); // prevent default link behavior so page doesn't jump
+    
+    for (let i = 0; i < paginationItems.length; i++) { // loop through all a elements in pagination and remove active class
+        paginationItems[i].classList.remove("active");
+    }
+
+    event.target.className = "active"; // set target class to active
+    
+    // determine page range by button clicked
+    let pageBtn = parseInt(event.target.innerHTML);
+    let pageEnd = pageBtn * 10;
+    let pageStart = pageEnd - 10;
+
+    // hide all students
+    for (let j = 0; j < students.length; j++) { 
+        students[j].style.display = "none";
+    } 
+    // show the 10 students based on page range
+    for (let p = pageStart; p < pageEnd; p++) {
+        if (!students[p]) {
+            break;
+        } else {
+            students[p].style.display = "block";
+        }
+    }
+
+});
